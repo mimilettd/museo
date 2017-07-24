8ae1a25a49e4db328035fec607555f4cbf6e0df4
mimilettd <mimilettd@gmail.com>
2017-07-24 09:19:10 -0600

    add museum id to photograph class

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,8 +1,10 @@
 class Photograph
   attr_reader :name,
-              :id
+              :artist_id,
+              :museum_id
   def initialize(hash={})
     @name = hash[:name]
-    @id = hash[:artist_id]
+    @artist_id = hash[:artist_id]
+    @museum_id = hash[:museum_id]
   end
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -9,13 +9,19 @@ class PhotographTest < Minitest::Test
     assert_instance_of Photograph, photograph
   end
 
-  def test_photograph_name
+  def test_photograph_has_name
     photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
   end
 
-  def test_photograph_artist_id
+  def test_photograph_has_artist_id
     photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
-    assert_equal 4, photograph.id
+    assert_equal 4, photograph.artist_id
   end
+
+  def test_photograph_has_museum_id
+    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    assert_equal 2, photograph.museum_id
+  end
+
 end
