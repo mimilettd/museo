c04bf43d96a9b379619b189c54410286c11fd52b
mimilettd <mimilettd@gmail.com>
2017-07-24 09:23:21 -0600

    test curator class exist

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -4,4 +4,8 @@ require 'minitest/pride'
 require './lib/curator'
 
 class CuratorTest < Minitest::Test
+  def test_class_exist
+    curator = Curator.new
+    assert_instance_of Curator, curator
+  end
 end
