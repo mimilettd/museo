48ef5992483c261315e9728f48e978b2fbc50c54
mimilettd <mimilettd@gmail.com>
2017-07-24 09:28:14 -0600

    update id in tests

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -10,27 +10,27 @@ class PhotographTest < Minitest::Test
   end
 
   def test_photograph_has_name
-    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
   end
 
   def test_photograph_has_artist_id
-    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal 4, photograph.artist_id
   end
 
   def test_photograph_has_museum_id
-    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal 2, photograph.museum_id
   end
 
   def test_photograph_has_a_year
-    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal 1954, photograph.year
   end
 
   def test_photograph_has_an_id
-    photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+    photograph = Photograph.new({id: 1, name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
     assert_equal 1, photograph.id
   end
 
